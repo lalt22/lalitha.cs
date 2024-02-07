79b224f95245f07169e64f6678cc72c3ad68f7c4
lalt22 <lalitha2seshadri@gmail.com>
2024-02-07 20:17:41 +1100

    Changed img src

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -1,9 +1,10 @@
 import styles from "./About.module.scss";
+import profile from "../../assets/profile.png";
 
 const About = () => {
     return (
         <section id="about-me" className={styles.about}>
-            <img src="src\assets\profile.png"/>
+            <img src={profile}/>
             <div className={styles.about_text}>
                 <h1>Hi, I'm Lalitha.</h1>
                 <p className={styles.about_p}>A Software Engineering student aspiring to become a full-stack developer. I'm passionate about solving problems, framing new ideas into real applications, and immersing myself in everything in Computer Science, from the bleeding edge to the foundations.
